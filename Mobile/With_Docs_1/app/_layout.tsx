93c60f7386d77349fa0100abd62fe736853594c5
Devesh Yadav <dd4168@srmist.edu.in>
2026-04-10 15:56:54 +0530

    Build a simple app with Expo_docs

--- Mobile/With_Docs_1/app/_layout.tsx
+++ Mobile/With_Docs_1/app/_layout.tsx
@@ -1,11 +1,15 @@
 // Layout file defines the shared-UI for all screens, like headers and tab bars
 
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
