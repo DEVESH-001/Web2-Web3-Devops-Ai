57c765087ed47be4e2bdea6f5b440a51588cc571
Devesh Yadav <dd4168@srmist.edu.in>
2026-04-21 13:07:48 +0530

    Implement GSAP animations in 1-basic-starter.tsx

--- Web/2_gsap/src/app/(experiments)/1-gsap-basics/starter.tsx
+++ Web/2_gsap/src/app/(experiments)/1-gsap-basics/starter.tsx
@@ -1,9 +1,74 @@
 "use client";
 
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+
+gsap.registerPlugin(SplitText);
+
 export default function Page() {
+  const containerRef = useRef<HTMLDivElement>(null); // ref to the container
+
+  // This is the boiler-plate code for GSAP
+  // useEffect(() => {
+  //   // tween => Tween is what does all the animation work
+  //   // const tween = gsap.from(".title", {
+  //   //   duration: 10,
+  //   //   x: 200, // start 200px to the right
+  //   //   // opacity: 0, // start with opacity 0
+  //   //   // onUpdate: () => {
+  //   //   //   console.log("updating");
+  //   //   // },
+  //   // });
+
+  //   // context => Context is what manages the lifecycle of the animation
+  //   const ctx = gsap.context(() => {
+  //     // context => Context is what manages the lifecycle of the animation
+  //     gsap.to(".title", {
+  //       x: 200,
+  //       duration: 10,
+  //     });
+
+  //     gsap.from(".title", {
+  //       opacity: 200,
+  //       duration: 10,
+  //     });
+  //   }, containerRef);
+
+  //   return () => {
+  //     //tween.revert();
+  //     ctx.revert(); // clean up the animation
+  //   };
+  // }, []);
+
+  //Gsap created a hook
+  useGSAP(
+    () => {
+      SplitText.create(".title", {
+        type: "chars,words",
+        charsClass: "letter",
+      });
+
+      gsap.to(".title .letter", {
+        y: 200,
+        opacity: 0,
+        ease: "circ.out",
+        duration: 10,
+        stagger: 0.03, // delay between each letter
+      });
+    },
+    {
+      scope: containerRef,
+    },
+  );
   return (
     <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
+      <p className="title">Check the console for updates</p>
+      <div
+        ref={containerRef}
+        className="flex h-screen items-end justify-left overflow-hidden"
+      >
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
           GSAP
           <br />
